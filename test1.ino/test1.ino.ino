dcf1d2ba21718e35b3f95fd9fc3d4bbabda3f20d
ChenThongpoom <61011353@kmitl.ac.th>
2019-11-08 01:02:37 -0600

    addArduino

---
+++ test1.ino/test1.ino.ino
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <math.h> 
+
+const int pingPin = 13;
+const int forward = 7, forward2 = 5;
+const int backward =6, backward2 = 4;
+int inPin = 12;
+int sum, ans;
+long duration, cm;
+
+void setup() {
+Serial.begin(9600);
+pinMode(forward, OUTPUT);
+pinMode(forward2, OUTPUT);
+pinMode(backward, OUTPUT);
+pinMode(backward2, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.read() == 'a'){
+    measure();
+  }
+  else if(Serial.read() == 'u'){
+    linearUp();
+  }
+  else if (Serial.read() == 'd'){
+    linearDown();
+  }
+  else if(Serial.read() == 'q'){
+    linearStop();
+  }
+
+
+}
+
+
+void linearUp(){
+  digitalWrite(forward,HIGH);
+  digitalWrite(forward2,HIGH);
+  digitalWrite(backward,LOW);
+  digitalWrite(backward2,LOW);
+  Serial.println("u");
+}
+
+
+void linearDown(){
+  digitalWrite(forward,LOW);
+  digitalWrite(forward2,LOW);
+  digitalWrite(backward,HIGH);
+  digitalWrite(backward2,HIGH);
+  Serial.println("d");
+}
+
+
+void linearStop(){
+  digitalWrite(forward,LOW);
+  digitalWrite(forward2,LOW);
+  digitalWrite(backward,LOW);
+  digitalWrite(backward2,LOW);
+  Serial.println("q");
+}
+
+
+void measure(){
+
+  
+  pinMode(pingPin, OUTPUT);
+
+
+digitalWrite(pingPin, LOW);
+delayMicroseconds(2);
+digitalWrite(pingPin, HIGH);
+delayMicroseconds(5);
+digitalWrite(pingPin, LOW);
+pinMode(inPin, INPUT);
+duration = pulseIn(inPin, HIGH);
+cm = microsecondsToCentimeters(duration);
+Serial.println(cm);
+//
+//if (cm > 40 and cm < 70)
+//{
+////  Serial.write("keep");
+////  Serial.print(cm);
+//  ans = calculate();
+//  Serial.println(ans);
+//}
+//else
+//{
+//  Serial.println("stop");
+////  Serial.print(cm);
+//}
+  delay(500);
+}
+
+
+   
+int calculate() {
+  
+  sum = (sin(20)/cos(20))*cm;
+  
+  printf("Range = %lf\n", cm);
+  return (sum);
+}
+
+
+long microsecondsToCentimeters(long microseconds)
+{
+// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+// The ping travels out and back, so to find the distance of the
+// object we take half of the distance travelled.
+return microseconds / 29 / 2;
+}
