bb343caf120954111b885a199e1a2069e8e28f92
tagun9921 <42992928+tagun9921@users.noreply.github.com>
2019-12-09 23:27:44 +0700

    Final Serial Program
    moveLinear() is made by Thongpoom which is used to control the linear actuators.
    distanceUs1() is made by Masapol which is used to detect the distance between the monitor and the user.

---
+++ SerialFinal.py
@@ -0,0 +1,76 @@
+import serial
+import time
+import math
+import sys
+import threading
+import queue
+from num2words import num2words
+import subprocess as sp
+
+
+ser = serial.Serial(
+                port='/dev/ttyACM0',
+                baudrate = 9600,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=1)
+
+
+
+def moveLinear(cond):
+    
+    if cond == 'up':
+        ser.write(str.encode('u'))
+        time.sleep(0.05)
+        
+        return
+    elif cond == 'down':
+        ser.write(str.encode('d'))
+        time.sleep(0.05)
+        
+        return
+    elif cond == 'stop':
+        ser.write(str.encode('q'))
+        time.sleep(0.05)
+        
+        return 
+    return "done"
+
+
+
+def distanceUs1():
+    x = 0
+    Sum = 0
+    count = 1
+    countDis = 1
+
+    
+    print('Distance detecting...')
+    sp.Popen(["aplay /home/pi/Documents/Group4_SMART_TABLE/soundForSOT/Disdetect.wav 2>/dev/null"], shell=True)
+
+    while count <= 3:
+        ser.write(str.encode('a'))
+        line = ser.readline()
+        if line.decode() != '':
+            if int(line.decode()) > 40 and int(line.decode()) < 70:
+                Sum += (math.tan(20))* int(line.decode())
+                count += 1
+            elif int(line.decode()) < 40:
+                countDis += 1
+                if countDis % 5 == 0:
+                    print("close")
+                    sp.Popen(["aplay /home/pi/Documents/Group4_SMART_TABLE/soundForSOT/Close.wav 2>/dev/null"], shell=True)
+                    
+
+            elif int(line.decode()) > 70:
+                countDis += 1
+                if countDis % 5==0:
+                    print("Far")
+                    sp.Popen(["aplay /home/pi/Documents/Group4_SMART_TABLE/soundForSOT/Far.wav 2>/dev/null"], shell=True)
+                    
+                                      
+    x = Sum // 3
+    return(int(x))
+    
+ 
